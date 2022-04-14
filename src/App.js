7a2d296e6feea2b5bc03a658ddf0e11aac12b4e1
arnausens <arnaust3n11@gmail.com>
2022-04-14 20:05:47 +0200

    Agregadas las rutas con las paginas principales

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import { Counter } from './features/counter/Counter';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from "react";
+import { makeStyles } from '@material-ui/core';
+import Login from "./Pages/Login";
+import Profile from "./Pages/Profile";
+import Paypal from "./Pages/Paypal";
+import Home from "./Pages/Home";
 
 function App() {
+    const user = "null";
+    const classes = useStyles();
     return (
-        <div className="App">
-            <h1>Empezamos</h1>
+        <div className={classes.root}>
+            <Router>
+                {
+                    !user ? (<Login/>) : (
+                        <Routes>
+                            <Route path='/profile' element={<Profile/>}/>
+                            <Route path='/checkout' element={<Paypal/>}/>
+                            <Route exact path="/" element={<Home/>}/>
+                        </Routes>
+                    )
+                }
+            </Router>
         </div>
     );
 }
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        // background: "#111",
+        minHeight: "100vh"
+    }
+}))
 
 export default App;
